2e92d18a0b68c8af760a9ecb0420a660a46ff948
Cahya Wirawan <cahya.wirawan@gmail.com>
2021-09-07 08:06:40 +0000

    bs=4, logging_step=5000, --adafactor, lr=0.0005

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -15,16 +15,17 @@ export WANDB_LOG_MODEL="true"
     --dataset_data_dir="/media/storage/datasets/collection" \
     --do_train --do_eval \
     --block_size="512" \
-    --per_device_train_batch_size="8" \
-    --per_device_eval_batch_size="8" \
-    --learning_rate="0.001" --warmup_steps="1000" \
+    --per_device_train_batch_size="4" \
+    --per_device_eval_batch_size="4" \
+    --learning_rate="0.0005" --warmup_steps="1000" \
     --adam_beta1="0.9" --adam_beta2="0.98" --weight_decay="0.01" \
     --overwrite_output_dir \
-    --num_train_epochs="20" \
+    --num_train_epochs="10" \
     --dataloader_num_workers="64" \
     --preprocessing_num_workers="64" \
-    --logging_steps="2500" \
-    --save_steps="2500" \
-    --eval_steps="2500" \
+    --logging_steps="5000" \
+    --save_steps="5000" \
+    --eval_steps="50000" \
     --validation_split_percentage="1" \
-    --push_to_hub
+    --adafactor \
+    --push_to_hub="false"
